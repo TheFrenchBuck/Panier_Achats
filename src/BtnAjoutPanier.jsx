a67234b73e48534658ae6010843d5215e2a8f057
Arsenault_Zachary <e1937017@cmaisonneuve.qc.ca>
2022-02-10 10:00:28 -0500

    Css premiere pause

--- src/BtnAjoutPanier.jsx
+++ src/BtnAjoutPanier.jsx
@@ -1,7 +1,10 @@
+import { Badge } from '@mui/material';
 import './BtnAjoutPanier.scss';
 export default function BtnAjoutPanier() { 
     return(
-        <button>Ajouter au panier</button>
+        <Badge badge badgeContent="2" color="primary">
+        <button className="BtnAjoutPanier">Ajouter au panier</button>
+        </Badge>
     );
 
 }
--- src/Produit.jsx
+++ src/Produit.jsx
@@ -1,5 +1,5 @@
 import './Produit.scss';
-
+import BtnAjoutPanier from './BtnAjoutPanier';
 export default function Produit(props) {
     // console.log(props);
     // console.log('Nom du produit ', props.nom)
@@ -8,7 +8,7 @@ export default function Produit(props) {
             <img src={"images-produits/" + props.pid + ".webp"} alt={props.nom} />
             <div className="titre">{props.nom}</div> 
             <div className="prix">{props.prix}</div>
-            
+            <BtnAjoutPanier />
         </article>
     );
 }
